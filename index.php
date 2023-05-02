83739884da94ab0fa518b291d82d3080c49d3202
imduckzz <mauricio.solarte0111@gmail.com>
2023-05-02 15:04:17 -0500

    Actualización de datos

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>My First App</title>
+</head>
+
+<body>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Identificator</label>
+    <input type="text" class="form-control" id="identificator" name="identificator">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Name</label>
+    <input type="text" class="form-control" id="nombre" name="nombre">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Detail</label>
+    <input type="password" class="form-control" id="detail">
+  </div>
+  <button type="submit" class="btn btn-primary">Registrar</button>
+</form>
+</body>
+</html>
